5b9847229218d6417445bf0eeac7d9c9e32aeb38
Wachirawit Tanleng <134188500+X-zeed@users.noreply.github.com>
2026-02-24 15:08:15 +0700

    Add relay control script using Modbus
    Implement Modbus relay control using pymodbus.

---
+++ BSM/WIndow/relay.py
@@ -0,0 +1,24 @@
+from pymodbus.client import ModbusSerialClient
+import time
+
+client = ModbusSerialClient(
+    port="COM3",
+    baudrate=9600,
+    parity="N",
+    stopbits=1,
+    bytesize=8,
+    timeout=1
+)
+
+relay=0
+
+print("Connect:", client.connect())
+
+print("Relay ON")
+client.write_coil(relay, True, device_id=1)
+time.sleep(2)
+
+print("Relay OFF")
+client.write_coil(relay, False, device_id=1)
+
+client.close()
